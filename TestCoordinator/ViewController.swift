e4818361b8fc26d7c1edddd24dc90d51dab90328
wodud1107 <wodud7992@naver.com>
2025-09-18 20:56:30 +0900

    Initial Commit

---
+++ TestCoordinator/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TestCoordinator
+//
+//  Created by 김재영 on 9/18/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
